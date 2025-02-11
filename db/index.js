4f2126c14657c5a77563bb4ba44b415812aeadb0
amankumarsinghhh <amankumar09jul@gmail.com>
2025-02-11 22:14:09 +0530

    feat: added openai integration

--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-dotenv.config();
-const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL);
